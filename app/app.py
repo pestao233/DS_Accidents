f051951cc9964a562e9a8ad9d7e3f71840d2584f
pestao233 <pestao@gmail.com>
2025-09-30 23:02:35 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,24 +2,40 @@ import streamlit as st
 
 # Configuration de la page
 st.set_page_config(
-    page_title="Accidents routiers - DS",
+    page_title="Accidents routiers - DataScientest - Cohorte DS JUL25",
     page_icon="🚧",
     layout="wide"
 )
 
 # Titre principal
-st.title("🚧 Accidents routiers en France — Projet DataScientest")
+st.title("🚧 Accidents routiers en France — Projet DataScientest / Equipe Enora Lever + Philippe Afonso")
 st.caption("Démo Streamlit — exploration, modélisation, prédiction")
 
 # Message d'accueil
 st.markdown("""
-Bienvenue dans notre application **Streamlit** 🎉
+Présentation du Projet sur **Streamlit** 
 
 Utilisez le menu de gauche pour naviguer :
-- **Exploration** : charger un CSV d'exemple, explorer les variables et la cible
-- **Modélisation** : entraîner un modèle baseline (RandomForest / HGB) et afficher les métriques
-- **Prédiction** : faire une prédiction sur un fichier CSV
+- **Exploration des données (variables explicatives + cible)** 
+- **Préparation des données**
+- **Modélisation** (entraînement de modèles RandomForest / HGB / .. + affichage de métriques)
+- **Prédiction** : faire une prédiction sur de nouvelles données
 """)
 
-# Petit message d'aide
-st.info("Astuce : si aucun modèle n'est présent dans `models/`, allez dans la page *Modélisation* pour en créer un rapidement.")
+# Contexte
+st.write("La sécurité routière constitue un enjeu majeur de santé publique et économique.
+En France, plusieurs dizaines de milliers d’accidents corporels surviennent chaque année, provoquant des blessures, des décès et des coûts importants pour la société.
+L’amélioration de la compréhension des facteurs influençant la gravité des accidents permettrait d’orienter les politiques publiques et  de mieux cibler les actions de sensibilisation et les campagnes de prévention.
+Dans ce cadre, notre projet vise à analyser et à modéliser les données d’accidents routiers afin de prédire la gravité d’un accident à partir de variables explicatives liées au contexte, aux usagers et aux véhicules impliqués.
+La problématique est un problème de classification supervisée : prédire une variable catégorielle (gravité) à partir de données hétérogènes (environnement, usagers, véhicules).
+")
+
+# Objectifs
+st.write("L’objectif principal de ce projet est de mettre en place un pipeline complet de Data Science comprenant :
+●	L’exploration et compréhension des données mises à disposition par le ministère de l’intérieur.
+●	L’identification des variables pertinentes et pré-traitement des données.
+●	La préparation d’un jeu de données exploitable pour une phase ultérieure de modélisation.
+Notre démarche est donc progressive :
+1.	partir d’une exploration descriptive des données,
+2.	poursuivre avec un travail de visualisation et de pré-processing,
+3.	avant d’aborder la modélisation proprement dite.")
