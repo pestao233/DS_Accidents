fae869f77dbce093f018df7cfff6a8a9024c73be
pestao233 <pestao@gmail.com>
2025-10-05 18:46:50 +0200

    Improvements Predictions.py

--- app/pages/3_Prediction.py
+++ app/pages/3_Prediction.py
@@ -29,18 +29,17 @@ model = joblib.load("models/model_HGBC.joblib")
 st.write("Choisissez ou modifiez quelques variables ci-dessous pour tester le modèle :")
 
 # Sélectionner une ligne de base
-index = st.number_input("Numéro de ligne de référence", min_value=0, max_value=len(df)-1, value=0)
+index = st.number_input("Numéro de ligne de référence", min_value=1, max_value=len(df)-1, value=1)
 row = df.iloc[index].copy()
 
 st.write("Voici la ligne choisie:")
 st.dataframe(row)
 
-# Exemple : quelques variables à modifier (adapte les noms à ton jeu)
+# Exemple : quelques variables à modifier
 lum = st.selectbox("Luminosité", sorted(df["lum"].unique()), index=int(row["lum"]) if "lum" in df.columns else 0)
 #atm = st.selectbox("Conditions météo", sorted(df["atm"].unique()), index=int(row["atm"]) if "atm" in df.columns else 0)
 secu = st.selectbox("Moyen sécurité", sorted(df["secu"].unique()), index=int(row["secu"]) if "secu" in df.columns else 0)
 col = st.selectbox("Type de collision", sorted(df["col"].unique()), index=int(row["col"]) if "col" in df.columns else 0)
-#vitesse = st.slider("Vitesse du véhicule (km/h)", 0, 200, int(row["vitesse"]) if "vitesse" in df.columns else 50)
 
 # Mettre à jour la ligne avec les valeurs choisies
 if "lum" in df.columns:
@@ -49,8 +48,6 @@ if "secu" in df.columns:
     row["secu"] = secu
 if "col" in df.columns:
     row["col"] = col
-#if "vitesse" in df.columns:
-#    row["vitesse"] = vitesse
 
 # Bouton de prédiction
 if st.button("Lancer la prédiction"):
